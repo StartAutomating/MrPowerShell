0efa9c41c68b381eaf863eb9cbce10e7e01c6005
James Brundage <+@noreply.github.com>
2025-04-19 14:59:03 -0700

    feat: Pure PowerShell Publishing ( Fixes #12, Fixes #13 )

---
+++ MrPowerShell.com/build.ps1
@@ -0,0 +1,68 @@
+param([string]$FilePattern = '\.(?>css|csv|html?|js|json|svg|rss|xml)\.ps1$')
+
+$filesToMake = 
+    @(Get-ChildItem -Recurse -Filter "*.*.ps1" |
+    Where-Object Name -Match $filePattern)
+
+$scriptsToMake = 
+    foreach ($file in $filesToMake) {
+        $executionContext.InvokeCommand.GetCommand($file.FullName,'ExternalScript')
+    }
+
+
+function layout {
+    "<html>"
+    "<head>"
+        "<title>$($NameWithoutExtension)</title>"
+    "</head>"
+    "<body>"
+    $input -join [Environment]::NewLine
+    "</body>"
+    "</html>"
+}
+
+
+$buildTimerStart = [DateTime]::Now
+foreach ($scriptToMake in $scriptsToMake) {
+    $scriptRoot = $scriptToMake.Source | Split-Path
+    Push-Location $scriptRoot
+    $fileOutputPath = $scriptToMake.Source -replace '\.ps1'
+    $title = ($scriptToMake.Source | Split-Path -Leaf) -replace $filePattern
+    if ($file.Directory.Name -eq 
+        $title
+    ) {
+        $indexInstead = $file.Name -replace ([Regex]::Escape($title)), 'index' -replace '\.ps1$'
+        $fileOutputPath = $indexInstead
+    }
+    $fileOutput = . $scriptToMake
+    if ($fileOutput -is [IO.FileInfo] -or 
+        $fileOutput -is [Collections.IList] -and $fileOutput -as [IO.FileInfo[]]
+    ) {
+        $fileOutput
+    } 
+    elseif ($fileOutput -is [xml]) {
+        $fileOutput.Save($fileOutputPath)
+        if ($?) {
+            Get-Item -Path $fileOutputPath
+        }
+    }
+    else {
+        if ($fileOutputPath -match '\.html$' -and -not ($fileOutput -match '<html>')) {
+            # Frame the HTML
+            $fileOutput | layout > $fileOutputPath
+        } else {
+            $fileOutput > $fileOutputPath
+        }
+        
+        if ($?) {
+            Get-Item -Path $fileOutputPath
+        }
+    }
+    
+    Pop-Location
+}
+
+$buildTimerEnd = [DateTime]::Now
+$buildTimeSpan = $buildTimerEnd - $buildTimerStart
+
+Write-Host "Built $($filesToMake.Length) files in $buildTimeSpan" -ForegroundColor Cyan
