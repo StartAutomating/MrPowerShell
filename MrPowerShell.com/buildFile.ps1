bf686d002eab1a5b29e8d7db445b7f2a6a323f92
James Brundage <+@noreply.github.com>
2025-04-19 16:26:59 -0700

    fix: Refactoring build
    Moving into files

---
+++ MrPowerShell.com/buildFile.ps1
@@ -0,0 +1,53 @@
+param(
+[Parameter(ValueFromPipeline)]
+[IO.FileInfo]
+$File
+)
+
+Set-Alias layout "$pwd/layout.ps1"
+
+$start = [datetime]::Now
+
+:nextFile foreach ($file in $input) {
+    $outFile = $file.FullName -replace '\.ps1$'
+    $Output = switch ($file.Extension) {
+        '.md' {
+            $title = $file.Name
+            $outFile = $file.FullName -replace '\.md$', '.html'
+            (ConvertFrom-Markdown -Path $file.FullName).Html |
+                layout
+        }
+        '.ps1' {
+            if ($file.Name -notmatch '\..+?\.ps1$') {
+                continue nextFile
+            }
+            . $file
+        }
+    }
+
+    if ($null -eq $Output) {
+        continue nextFile
+    }
+    
+    if ($output -is [xml]) {
+        $output.Save($outFile)
+        if ($?) {
+            Get-Item -Path $outFile
+        }
+    } elseif ($output -is [IO.FileInfo] -or (
+        $output -is [Collections.IList] -and $output -as [IO.FileInfo[]]
+    )) {
+        $output
+    } else {
+        if ($outFile -match '\.html$' -and -not ($output -match '<html>')) {
+            $output | layout > $outFile
+        } else {
+            $output > $outFile
+        }
+        if ($?) {
+            Get-Item -Path $outFile
+        }
+    }
+}
+
+$end = [datetime]::Now
---
+++ MrPowerShell.com/layout.ps1
@@ -0,0 +1,18 @@
+$paletteName = 'Konsolas'
+
+$argsAndinput = @($args) + @($input)
+
+@"
+<html>
+    <head>
+        <title>$Title</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/$PaletteName.css'>
+        $OpenGraph
+        $ImportMap
+    </head>
+    <body>
+$($argsAndinput -join [Environment]::NewLine)
+    </body>
+<html>
+"@
