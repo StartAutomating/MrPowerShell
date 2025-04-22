a83bf7523f7e3c8ab72a23aedbdc6b94cb607f6e
James Brundage <+@noreply.github.com>
2025-04-22 13:42:55 -0700

    test: Adding ./buildCommands.json

---
+++ MrPowerShell.com/buildCommands.json.ps1
@@ -0,0 +1,7 @@
+[Ordered]@{
+    Applications = Get-Command -CommandType Application | select Name, source
+    Functions = Get-Command -CommandType Function | select Name, Module
+    Cmdlets = Get-Command -CommandType Cmdlets | select Name, Module
+    Aliases = Get-Command -CommandType Alias | select Name, ResolvedCommand
+} | ConvertTo-Json -Depth 3
+
