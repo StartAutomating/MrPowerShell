2458013fc39404913fbdc8e8b759c96c55a0f504
James Brundage <+@noreply.github.com>
2025-04-22 16:20:19 -0700

    test: Adding ./buildCommands.json
    Only exporting name

--- MrPowerShell.com/buildCommands.json.ps1
+++ MrPowerShell.com/buildCommands.json.ps1
@@ -1,7 +1,7 @@
 [Ordered]@{
-    Applications = Get-Command -CommandType Application | select Name, source
-    Functions = Get-Command -CommandType Function | select Name, Module
-    Cmdlets = Get-Command -CommandType Cmdlet | select Name, Module
-    Aliases = Get-Command -CommandType Alias | select Name, ResolvedCommand
+    Applications = Get-Command -CommandType Application | select -ExpandProperty Name
+    Functions = Get-Command -CommandType Function | select -ExpandProperty Name
+    Cmdlets = Get-Command -CommandType Cmdlet | select -ExpandProperty Name
+    Aliases = Get-Command -CommandType Alias | select -ExpandProperty Name
 } | ConvertTo-Json -Depth 3
 
