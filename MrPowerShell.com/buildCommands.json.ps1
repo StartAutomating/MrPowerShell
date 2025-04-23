817a094527de00a7723606ad2ece9abc5b03e9de
James Brundage <+@noreply.github.com>
2025-04-23 16:28:44 -0700

    fix: Moving .well-known/atproto to .txt file and other cleanup

--- MrPowerShell.com/buildCommands.json.ps1
+++
@@ -1,7 +0,0 @@
-[Ordered]@{
-    Applications = Get-Command -CommandType Application | select -ExpandProperty Name
-    Functions = Get-Command -CommandType Function | select -ExpandProperty Name
-    Cmdlets = Get-Command -CommandType Cmdlet | select -ExpandProperty Name
-    Aliases = Get-Command -CommandType Alias | select -ExpandProperty Name
-} | ConvertTo-Json -Depth 3
-
