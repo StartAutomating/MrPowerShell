f469c1adcf562b14e0a45543fe873cbfd31ae59c
James Brundage <+@noreply.github.com>
2025-04-23 16:14:54 -0700

    feat: Adding 404.html.ps1

--- MrPowerShell.com/FilesByTime.html.ps1
+++
@@ -1,5 +0,0 @@
-Get-ChildItem -Path ($PSScriptRoot | Split-Path) -Recurse -File | 
-    Sort-Object CreationTime -Descending | 
-    Select-Object Name, CreationTime |
-    ConvertTo-Html -Fragment
-
