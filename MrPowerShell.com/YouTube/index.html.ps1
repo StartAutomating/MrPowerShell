e55ee7f1808a1d876b4aa771f7b759f0f512a21a
James Brundage <+@noreply.github.com>
2025-04-24 15:48:45 -0700

    feat: oEmbedding YouTube videos
    Pushing into directory

--- MrPowerShell.com/YouTube/index.html.ps1
+++ MrPowerShell.com/YouTube/index.html.ps1
@@ -1,6 +1,8 @@
 #requires -Module oEmbed
+Push-Location $PSScriptRoot
 ./index.json.ps1 |    
     ForEach-Object { 
         Get-OEmbed -Url $_.YouTubeUrl -MaxHeight 1280 -MaxWidth 720
     } | 
     Select-Object -ExpandProperty Html
+Pop-Location
