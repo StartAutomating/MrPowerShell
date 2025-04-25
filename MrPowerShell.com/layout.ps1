0f2f157efa3a89160aafb66d45da2af89df36426
James Brundage <+@noreply.github.com>
2025-04-25 15:54:02 -0700

    style: Updating layout (using Highlight.js)

--- MrPowerShell.com/layout.ps1
+++ MrPowerShell.com/layout.ps1
@@ -17,11 +17,20 @@ body {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/$PaletteName.css' id='palette' />
         <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' id='font' />
+        
         $OpenGraph
         $ImportMap
         <style>
 $style        
         </style>
+        $(
+            @(
+                '<script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/highlight.min.js"></script>'
+                foreach ($language in 'PowerShell') {
+                    "<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/languages/$language.min.js'></script>"
+                }
+            ) -join [Environment]::NewLine            
+        )
     </head>
     <body>
 $($argsAndinput -join [Environment]::NewLine)
