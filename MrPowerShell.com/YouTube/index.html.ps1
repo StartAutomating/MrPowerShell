5c85f145b72d6258ae79cfb2d9380fd2bfd9962a
James Brundage <+@noreply.github.com>
2025-04-24 21:29:58 -0700

    feat: oEmbedding YouTube videos
    Using css grid

--- MrPowerShell.com/YouTube/index.html.ps1
+++ MrPowerShell.com/YouTube/index.html.ps1
@@ -1,8 +1,34 @@
 #requires -Module oEmbed
 Push-Location $PSScriptRoot
-./index.json.ps1 |    
+@"
+<style>
+.grid-container {
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;    
+}
+.grid-center {
+    text-align: center;
+    grid-column: 2;
+    padding: 2em;
+}
+</style>
+"@
+"<div class='grid-container'>"
+./index.json.ps1 |
+    Sort-Object @{
+        Expression = "Year"
+        Descending=$true
+    }, @{
+        Expression="Name"
+        Descending = $false
+    } |   
     ForEach-Object { 
         Get-OEmbed -Url $_.YouTubeUrl -MaxHeight 1280 -MaxWidth 720
     } | 
-    Select-Object -ExpandProperty Html
+    ForEach-Object {
+        "<div class='grid-center'>"
+        $_.html
+        "</div>"
+    }
+"</div>"
 Pop-Location
