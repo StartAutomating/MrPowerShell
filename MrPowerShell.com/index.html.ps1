77ca62f11f5d4c097aa86ae8569f5f59c7194710
James Brundage <+@noreply.github.com>
2025-04-24 21:47:09 -0700

    fix: Fixing social link

--- MrPowerShell.com/index.html.ps1
+++ MrPowerShell.com/index.html.ps1
@@ -1,12 +1,3 @@
-"<html>"
-"<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/Konsolas.css'>"
-"<style>"
-"body { margin: 2em; }"
-"#header,#footer {"
-"    text-align: center;"
-"}"
-"</style>"
-"<body>"
 "<div id='header'>"
 "<h1><a href='/'>MrPowerShell</a></h1>"
 "<svg width='75%' height='25%'>"
@@ -18,7 +9,7 @@ $markdown = ConvertFrom-Markdown -Path "$psScriptRoot/Greetings.md"
 $markdown.Html
 "</div>"
 "<div id='footer'>"
-"<a href='https://bsky.app/profile/mrpowershell.bsky.social'>
+"<a href='https://bsky.app/profile/mrpowershell.com'>
 Follow me on BlueSky:
 <br/>
 <svg width='75%' height='10%'>
@@ -28,5 +19,3 @@ $((Get-Content -Path .\BlueSkyRainbow-Animated.svg -Raw) -replace '<\?xml.+>')
 <br/>
 <a href='BlueSkyRainbow-Animated.svg'>Download SVG</a>"
 "</div>"
-"</body>"
-"</html>"
