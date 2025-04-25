17182a58176adde2ac519d673683a52487f646a4
James Brundage <+@noreply.github.com>
2025-04-24 21:59:46 -0700

    fix: Fixing social link
    Updating style and structure

--- MrPowerShell.com/index.html.ps1
+++ MrPowerShell.com/index.html.ps1
@@ -1,15 +1,38 @@
-"<div id='header'>"
-"<h1><a href='/'>MrPowerShell</a></h1>"
-"<svg width='75%' height='25%'>"
-"$((Get-Content -Path .\MrPowerShell-Animated.svg -Raw) -replace '<\?xml.+>')"
-"</svg>"
-"</div>"
-"<div id='mainContent'>"
+$style = @"
+<style type='text/css'>
+.header {
+    text-align: center;
+    margin: 2em;
+}
+.content {        
+    margin: 3em;
+}
+.footer {
+    text-align: center;
+    margin: 2em;
+}
+</style>
+"@
+
+$header = @"
+<div class='header'>
+<h1><a href='/'>MrPowerShell</a></h1>
+<svg width='75%' height='25%'>
+$((Get-Content -Path .\MrPowerShell-Animated.svg -Raw) -replace '<\?xml.+>')
+</svg>
+</div>
+"@
+
+$content = @"
+<div class='content'>"
 $markdown = ConvertFrom-Markdown -Path "$psScriptRoot/Greetings.md"
 $markdown.Html
-"</div>"
-"<div id='footer'>"
-"<a href='https://bsky.app/profile/mrpowershell.com'>
+"</div>
+"@
+
+$footer = @"
+<div class='footer'>
+<a href='https://bsky.app/profile/mrpowershell.com'>
 Follow me on BlueSky:
 <br/>
 <svg width='75%' height='10%'>
@@ -18,4 +41,10 @@ $((Get-Content -Path .\BlueSkyRainbow-Animated.svg -Raw) -replace '<\?xml.+>')
 </a>
 <br/>
 <a href='BlueSkyRainbow-Animated.svg'>Download SVG</a>"
-"</div>"
+</div>
+"@
+
+$style,
+    $header,    
+    $content,
+    $footer -join [Enviroment]::NewLine
