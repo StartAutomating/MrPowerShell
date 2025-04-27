915388cd491b4286d9a7c97f88507d6fa71e8aae
James Brundage <+@noreply.github.com>
2025-04-27 12:00:17 -0700

    feat: Updating layout
    Adding font / codeFont variables

--- MrPowerShell.com/layout.ps1
+++ MrPowerShell.com/layout.ps1
@@ -1,14 +1,19 @@
 $paletteName = 'Konsolas'
+$Font = 'Roboto'
+$CodeFont = 'Inconsolata'
 
 $argsAndinput = @($args) + @($input)
 
-$style = @'
+$style = @"
 body {
     width: 100vw;
     height: 100vh;
-    font-family: 'Roboto', sans-serif;    
+    font-family: '$GoogleFont', sans-serif;        
 }
-'@
+pre, code {
+    font-family: '$CodeFont', monospace;
+}
+@
 
 @"
 <html>
@@ -16,7 +21,8 @@ body {
         <title>$Title</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/2bitdesigns/4bitcss@latest/css/$PaletteName.css' id='palette' />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto' id='font' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=$Font' id='font' />
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=$CodeFont' id='codeFont' />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@latest/build/styles/default.min.css" id='highlight' />
         $OpenGraph
         $ImportMap
