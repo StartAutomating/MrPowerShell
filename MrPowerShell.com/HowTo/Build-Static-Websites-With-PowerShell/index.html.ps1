dc5230ac3b5d64ebc107b6487a7a5481d641e543
James Brundage <+@noreply.github.com>
2025-05-03 13:53:45 -0700

    feat: Updating HowTo/Build-Static-Websites-With-PowerShell
    Using markdown more sparingly so we can show the full source

--- MrPowerShell.com/HowTo/Build-Static-Websites-With-PowerShell/index.html.ps1
+++ MrPowerShell.com/HowTo/Build-Static-Websites-With-PowerShell/index.html.ps1
@@ -6,9 +6,8 @@ $Title = "How to Build Static Websites with PowerShell"
 "<p>PowerShell is pretty powerful when it comes to text processing, and static sites are almost entirely text.</p>"    
 "<h2>Hello World</h2>"
 "<p>Let's start very simple, with a customizable Hello World</p>"
-(ConvertFrom-Markdown -InputObject "
-~~~PowerShell
-$({
+"<pre><code class='language-PowerShell'>"
+[Web.HttpUtility]::HtmlEncode({
     # Set our message
     $Message = "Hello World!"
     # Generate a string that embeds our message, and redirect it to a file
@@ -16,43 +15,22 @@ $({
     # use Invoke-Item to open the file in the default browser
     Invoke-Item ./MyFirstPowerShellPage.html
 })
-~~~
-").Html
-    "<p>When you run this code, it will create a file called MyFirstPowerShellPage.html in the current directory.</p>"
-    "<p>When you open that file in a browser, you'll see the text 'Hello World!' displayed.</p>"
-    "<h3>Once more, with Markdown</h3>"
-    "<p>PowerShell core includes a ConvertFrom-Markdown cmdlet, which can be used to generate HTML from Markdown.</p>"
-    "<p>Markdown is a simple markup language that can be used to format text.</p>"
-    $LearnLink = 'https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/convertfrom-markdown?wt.mc_id=MVP_321542'
-    "<p>Here's an example of how to use <a href='$LearnLink'>ConvertFrom-Markdown</a> to generate HTML:</p>"
-    (ConvertFrom-Markdown -InputObject "
-~~~PowerShell
-$({
+"</pre></code>"
 
-    $Markdown = "# Hello From Markdown"
-    # ConvertFrom-Markdown returns an object with a property called 'Html'
-    # convert our markdown to HTML, and redirect it to a file        
-    (ConvertFrom-Markdown -InputObject $Markdown).Html > HelloFromMarkdown.html
-    # use Invoke-Item to open the file in the default browser
-    Invoke-Item ./HelloFromMarkdown.html
-})
-~~~
-").Html
+"<p>When you run this code, it will create a file called MyFirstPowerShellPage.html in the current directory.</p>"
+"<p>When you open that file in a browser, you'll see the text 'Hello World!' displayed.</p>"
+"<h3>Once more, with Markdown</h3>"
+"<p>PowerShell core includes a ConvertFrom-Markdown cmdlet, which can be used to generate HTML from Markdown.</p>"
+"<p>Markdown is a simple markup language that can be used to format text.</p>"
+$LearnLink = 'https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/convertfrom-markdown?wt.mc_id=MVP_321542'
+"<p>Here's an example of how to use <a href='$LearnLink'>ConvertFrom-Markdown</a> to generate HTML:</p>"
+ConvertFrom-Markdown -InputObject "# Hello From Markdown!" |
+    Select-Object -ExpandProperty HTML
 
 "<p>Since GitHub Workflows allow us to run PowerShell scripts, this is all we need to do to build a static site.</p>"
-
 "<p>Now, let's get a little meta.  We're going to include the source for this page in this page</p>"
-
 "<p>To to this, we can use the <code>MyInvocation</code> automatic variable to get the script block for this page, like so:</p>"
-
-(ConvertFrom-Markdown -InputObject '
-~~~PowerShell
-$($myInvocation.MyCommand.ScriptBlock)
-~~~
-').Html
-
 "<p>The full code is below:</p>"
-
 "<hr/>"
 
 (ConvertFrom-Markdown -InputObject "
