086fb5e9c4df7906ad63802bc56dc19cfbb2d21b
James Brundage <+@noreply.github.com>
2025-05-03 13:37:01 -0700

    feat: Adding https://mrpowershell.com/Gists
    Putting gists in bullet points

--- MrPowerShell.com/Gists/index.html.ps1
+++ MrPowerShell.com/Gists/index.html.ps1
@@ -6,9 +6,12 @@ if (-not $script:myGists) {
     $script:myGists = Invoke-RestMethod -Uri "https://api.github.com/users/$GitHubUser/gists"    
 }
 
-
+"<ul>"
 $script:myGists | 
     Sort-Object updated_at -Descending |
     ForEach-Object {
-        "<a href='$($_.html_url)' target='_blank' class='gist'>$([Web.HttpUtility]::HtmlEncode($_.description))</a>"
+        "<li>"
+            "<a href='$($_.html_url)' target='_blank' class='gist'>$([Web.HttpUtility]::HtmlEncode($_.description))</a>"
+        "</li>"
     }
+"</ul>"
