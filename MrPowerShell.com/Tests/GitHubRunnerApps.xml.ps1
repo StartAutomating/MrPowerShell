e63416ea1b64e2f3290b9adf8c18cbd47e803d07
James Brundage <+@noreply.github.com>
2025-05-10 16:02:39 -0700

    test: GitHubRunnerApps.xml.ps1

---
+++ MrPowerShell.com/Tests/GitHubRunnerApps.xml.ps1
@@ -0,0 +1,11 @@
+$dataSet = [Data.DataSet]::new('GitHubRunnerApps') 
+$appTable = $dataSet.Tables.Add('App')
+$appTable.Columns.AddRange(@(
+    [Data.DataColumn]::new('Name', [string], '', 'Attribute'),
+    [Data.DataColumn]::new('Path', [string], '', 'Attribute'),
+    [Data.DataColumn]::new('Version', [Version], '', 'Attribute')
+))
+$null = foreach ($app in Get-Command -CommandType Application) {
+    $appTable.Rows.Add($app.Name, $app.Source, $app.Version)
+}
+$dataSet
