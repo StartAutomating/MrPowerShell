c0e68629da1f664a0f40d1f2caa07ac5bc02498c
James Brundage <+@noreply.github.com>
2025-05-10 15:25:47 -0700

    test: Moving /Test to /Tests and adding CodeBlock.xsd.ps1
    Fixing variable name

--- MrPowerShell.com/Tests/CodeBlock.xsd.ps1
+++ MrPowerShell.com/Tests/CodeBlock.xsd.ps1
@@ -1,5 +1,5 @@
 $codeBlockSchema = [Data.DataSet]::New('CodeBlocks')
-$codeTable = $codeBlocks.Tables.Add('code')
+$codeTable = $codeBlockSchema.Tables.Add('code')
 $codeTable.Columns.AddRange(@(
     [Data.DataColumn]::new('class', [string], '', 'Attribute')
     [Data.DataColumn]::new('content', [string], '', 'SimpleContent')
