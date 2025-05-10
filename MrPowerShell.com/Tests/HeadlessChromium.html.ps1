ce28e303579cbe955b0cbc3a2410f0e0f777dba2
James Brundage <+@noreply.github.com>
2025-05-10 16:31:50 -0700

    test: HeadlessChromium.html.ps1
    There will be only one

--- MrPowerShell.com/Tests/HeadlessChromium.html.ps1
+++ MrPowerShell.com/Tests/HeadlessChromium.html.ps1
@@ -1,15 +1,17 @@
 "<p> We can run chromium in headless mode inside of a GitHub action.</p>"
 
 #region Set up chromium alias
-$chromium = Get-Command -Name chromium -CommandType Application, Alias -ErrorAction Ignore
+$chromium = Get-Command -Name chromium -CommandType Application, Alias -ErrorAction Ignore | 
+    Select-Object -First 1
 if (-not $chromium) {
-    $chromePath = Get-Process chrome -ErrorAction Ignore | Select-Object -ExpandProperty Path
+    $chromePath = Get-Process chrome -ErrorAction Ignore |
+        Select-Object -ExpandProperty Path -First 1            
     if ($chromePath) {
         $chromium = Set-Alias -Name chromium -Value $chromePath -Force -PassThru        
     }
     else {
-        $edgePath = Get-Process msedge -ErrorAction Ignore | 
-            Select-Object -ExpandProperty Path
+        $edgePath = Get-Process msedge -ErrorAction Ignore |
+            Select-Object -ExpandProperty Path -First 1            
         if ($edgePath) {
             $chromium = Set-Alias -Name chromium -Value $edgePath -Force -PassThru
         }
