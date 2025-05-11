922441204523ae450bfd2298e504b31b28acffe4
James Brundage <+@noreply.github.com>
2025-05-10 18:38:26 -0700

    test: HeadlessChromium.html.ps1
    Having proved the test, now turning it into something like a post

--- MrPowerShell.com/Tests/HeadlessChromium.html.ps1
+++ MrPowerShell.com/Tests/HeadlessChromium.html.ps1
@@ -3,30 +3,40 @@ param(
     $ShowUri = 'https://mrpowershell.com/'
 )
 
-"<p> We can run chromium in headless mode inside of a GitHub action.</p>"
-
-#region Set up chromium alias
-$chromium = Get-Command -Name chromium -CommandType Application, Alias -ErrorAction Ignore | 
-    Select-Object -First 1
-if (-not $chromium) {
-    $chromePath = Get-Process chrome -ErrorAction Ignore |
-        Select-Object -ExpandProperty Path -First 1            
-    if ($chromePath) {
-        $chromium = Set-Alias -Name chromium -Value $chromePath -Force -PassThru        
-    }
-    else {
-        $edgePath = Get-Process msedge -ErrorAction Ignore |
-            Select-Object -ExpandProperty Path -First 1            
-        if ($edgePath) {
-            $chromium = Set-Alias -Name chromium -Value $edgePath -Force -PassThru
+"<p> We can run chromium in headless mode inside of a GitHub action </p>"
+"<pre><code class='language-powershell'>"
+{
+    #region Find Chromium
+    if (-not $script:chromium) {
+        # Find the chromium executable or alias, pick the first one
+        $chromium = Get-Command -Name chromium -CommandType Application, Alias -ErrorAction Ignore | 
+            Select-Object -First 1
+        # If we don't have a chromium alias, we'll try to find the chrome or edge executable.
+        if ($chromium) {
+            $script:chromium = $chromium
+        } else {
+            # If there's no running instance of chrome, we'll try to find it.
+            $chromePath = 
+                Get-Process chrome, msedge -ErrorAction Ignore |
+                    Select-Object -ExpandProperty Path -First 1        
+            if ($chromePath) {
+                $script:chromium   = $chromePath
+            }
+            else {                        
+                Write-Error 'Chromium not found. Please `Set-Alias chromium $ChromiumPath` (after you set $ChromiumPath)'
+            }
         }
-        else {
-            Write-Warning "No chromium or edge found in the path."
-            return
-        }
     }
+    # $script:chromium
+    #endregion Find Chromium
+    $headlessArguments = @(
+        '--headless', # run in headless mode
+        '--dump-dom', # dump the DOM to stdout
+        '--disable-gpu', # disable GPU acceleration
+        '--no-sandbox' # disable the sandbox if running in CI/CD    
+        $ShowUri       # the URL to show
+    )
+    $chromiumOutput = & $script:chromium @headlessArguments | Out-String
+    "<pre><code class='language-html'>$([Web.HttpUtility]::HtmlEncode("$chromiumOutput"))</code></pre>"
 }
-#endregion Set up chromium alias
-
-$chromiumOutput = & $chromium --headless --dump-dom --no-sandbox --disable-gpu "$ShowUri" | Out-String
-"<pre><code class='language-html'>$([Web.HttpUtility]::HtmlEncode("$chromiumOutput"))</code></pre>"
+"</code></pre>"
