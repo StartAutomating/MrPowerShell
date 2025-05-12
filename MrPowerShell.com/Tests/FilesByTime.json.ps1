3bac9ee4f54a151e24c749fbceb54dfb17e22af9
James Brundage <+@noreply.github.com>
2025-05-12 11:14:46 -0700

    test: Updating /Tests/FilesByTime.json
    Skipping changes at root, adding CommitMessage

--- MrPowerShell.com/Tests/FilesByTime.json.ps1
+++ MrPowerShell.com/Tests/FilesByTime.json.ps1
@@ -2,7 +2,9 @@
 $gitLog = git log -Statistics
 foreach ($entry in $gitLog) {
     foreach ($change in $entry.Changes) {
+        if ($change.FilePath -match '^\.') { continue }
         $change | 
-            Add-Member -MemberType NoteProperty -Name 'CommitDate' -Value $entry.CommitDate -Force -PassThru
+            Add-Member -MemberType NoteProperty -Name 'CommitDate' -Value $entry.CommitDate -Force -PassThru |        
+            Add-Member -MemberType NoteProperty -Name 'CommitMessage' -Value $entry.CommitMessage -Force -PassThru
     }
 }
