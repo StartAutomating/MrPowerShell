556314a5818f6158d5719bbab4119e1ef5221a45
James Brundage <+@noreply.github.com>
2025-05-18 17:43:40 -0700

    feat: Adding config.ps1, reading at proto data, and creating at.zip ( Fixes #25, Fixes #26 )

---
+++ MrPowerShell.com/config.ps1
@@ -0,0 +1,103 @@
+<#
+.SYNOPSIS
+    Configures the site
+.DESCRIPTION
+    Configures the site.  
+    
+    At the point this runs, a $Site dictionary should exist, and it should contain a list of files to build.
+
+    Any *.json, *.psd1, or *.yaml files in the root should have already been loaded into the $Site dictionary.
+
+    Any additional configuration or common initialization should be done here.
+#>
+
+#region At Protocol
+
+#region At Protocol Data
+
+# If we have a script root, we'll use it to set the working directory.
+if ($psScriptRoot) {Push-Location $psScriptRoot}
+
+# Create a new DataSet to hold the At Protocol data.
+$atProtocolData = [Data.DataSet]::new('AtProtocol')
+# Look up in the path
+$parentPath = $PSScriptRoot | Split-Path -Parent
+# Find any directories that start with did* in the parent path.
+$atJsonFiles = $parentPath | 
+    Get-ChildItem -Filter did* |
+    Get-ChildItem -Directory | 
+    Get-ChildItem -Filter *.json # and get any json files in them.    
+foreach ($jsonFile in $atJsonFiles) {
+    $jsonText = Get-Content -Path $jsonFile.FullName -Raw
+    $jsonObject = $jsonText | 
+        ConvertFrom-Json
+    $recordType = $jsonObject.commit.record.'$type'
+    if (-not $recordType) { continue }
+    
+    if (-not $atProtocolData.Tables[$recordType]) {
+        $dataTable = $atProtocolData.Tables.Add($recordType)
+        $dataTable.Columns.AddRange(@(
+            [Data.DataColumn]::new('did', [string], '', 'Attribute'),
+            [Data.DataColumn]::new('rkey', [string], '', 'Attribute')
+            [Data.DataColumn]::new('createdAt', [DateTime], '', 'Attribute'),
+            [Data.DataColumn]::new('atUri', [string], "'at://' + did + '/$recordType/' + rkey", 'Attribute')
+            [Data.DataColumn]::new('json', [string], '', 'SimpleContent')
+            [Data.DataColumn]::new('message', [object], '', 'Hidden')
+        ))
+        $dataTable.PrimaryKey = $dataTable.Columns['did', 'rkey']
+    } else {
+        $dataTable = $atProtocolData.Tables[$recordType]
+    }
+
+    $dataRow = $dataTable.NewRow()
+    $dataRow['did'] = $jsonObject.did
+    $dataRow['rkey'] = $jsonObject.commit.rkey    
+    $dataRow['createdAt'] = 
+        if ($jsonObject.commit.record.createdAt) { $jsonObject.commit.record.createdAt }
+        else { [DBNull]::Value }
+    $dataRow['message'] = $jsonObject
+    $dataRow['json'] = $jsonText
+    $dataRow.pstypenames.insert(0, $recordType)
+    $dataTable.Rows.Add($dataRow)
+}   
+
+if ($site -is [Collections.IDictionary]) {
+    $site.AtData = $atProtocolData
+}
+
+#endregion At Protocol Data
+
+#region at.zip 
+$atPackage = [IO.Packaging.Package]::Open("$pwd/at.zip", "OpenOrCreate", "ReadWrite")
+foreach ($dataTable in $atProtocolData.Tables) {
+    $dataSchemaPart = 
+        if (-not $atPackage.PartExists("/$($dataTable.TableName).xsd")) {
+            $atPackage.CreatePart("/$($dataTable.TableName).xsd", "application/xml", 'Maximum')    
+        } else {
+            $atPackage.GetPart("/$($dataTable.TableName).xsd")
+        }
+    $dataSchemaStream = $dataSchemaPart.GetStream()
+    $dataTable.WriteXmlSchema($dataSchemaStream)
+    $dataSchemaStream.Close()
+    $dataSchemaStream.Dispose()
+
+
+    $dataTablePart =
+        if (-not $atPackage.PartExists("/$($dataTable.TableName).xml")) {
+            $atPackage.CreatePart("/$($dataTable.TableName).xml", "application/xml", 'Maximum')    
+        } else {
+            $atPackage.GetPart("/$($dataTable.TableName).xml")
+        }
+
+    $dataTableStream = $dataTablePart.GetStream()
+    $dataTable.WriteXml($dataTableStream)
+    $dataTableStream.Close()
+    $dataTableStream.Dispose()
+} 
+$atPackage.Close()
+$atPackage.Dispose()
+
+if ($psScriptRoot) {Pop-Location}
+#endregion at.zip
+
+#endregion At Protocol
