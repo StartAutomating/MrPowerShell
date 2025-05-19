b4df8623ddb5acd7af5b96d49fd7622f1305754a
James Brundage <+@noreply.github.com>
2025-05-19 12:51:15 -0700

    test: Adding PagesByDate.json
    Collecting change history

--- MrPowerShell.com/Tests/PagesByDate.json.ps1
+++ MrPowerShell.com/Tests/PagesByDate.json.ps1
@@ -1,22 +1,25 @@
 #requires -Module ugit
 $gitLog = git log -Statistics
-Push-Location ($PSScriptRoot | Split-Path)
+Push-Location (git rev-parse --show-toplevel)
 $htmlOverTime = [Ordered]@{}
 foreach ($entry in $gitLog) {
     foreach ($change in $entry.Changes) {
         if ($change.FilePath -match '^\.') { continue }
-        if ($change.FilePath -notmatch '\.(?>html|md)') { continue }
-        if ($htmlOverTime[$change.FilePath]) {
+        if ($change.FilePath -notmatch '\.(?>html|md)') { continue }        
+        if (-not (Test-Path $change.FilePath -ErrorAction Ignore)) {
             continue
         }
-        if (-not (Test-Path $change.FilePath)) {
-            continue
+        if (-not $htmlOverTime[$change.FilePath]) {
+            $htmlOverTime[$change.FilePath] = @()
         }
-        $htmlOverTime[$change.FilePath] = [Ordered]@{
-            FilePath = $change.FilePath
+
+        $htmlOverTime[$change.FilePath] += [Ordered]@{            
             CommitDate = $entry.CommitDate
             CommitMessage = $entry.CommitMessage
-        }
+            LinesChanged = $change.LinesChanged
+            LinesInserted = $change.LinesInserted
+            LinesDeleted = $change.LinesDeleted
+        }        
     }
 }
 $htmlOverTime
