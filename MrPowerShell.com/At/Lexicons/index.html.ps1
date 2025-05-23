42e4e2ee412aed6a066c676ce02e0c19fa5f20d7
James Brundage <+@noreply.github.com>
2025-05-22 23:05:09 -0700

    feat: https://MrPowerShell.com/At/Lexicons ( Fixes #32 )

---
+++ MrPowerShell.com/At/Lexicons/index.html.ps1
@@ -0,0 +1,50 @@
+
+
+# We'll be populating a subdirectory with a sparse checkout of a repository.
+if (-not $psScriptRoot) { return }
+
+$atProtoPath = Join-Path ($PSScriptRoot | Split-Path | Split-Path | Split-Path) atproto
+
+Push-Location $PSScriptRoot
+# If we don't have the content, get it.
+if (-not (Test-Path $atProtoPath)) {
+    $gitOutput = @(
+        # use ugit to clone -Nothing
+        # git clone https://github.com/bluesky-social/atproto/ -Nothing
+        git clone --depth 1 --no-checkout --sparse --filter=tree:0 https://github.com/bluesky-social/atproto/ $atProtoPath
+        Push-Location $atProtoPath
+        # and then use a sparse-checkout to get only the CSS-related content.        
+        git sparse-checkout set --no-cone /lexicons/**/**.json
+        # checkout the content, and we're set.
+        git checkout
+        
+        Pop-Location
+    )
+}
+
+$AllLexicons = @()
+
+Get-ChildItem -Path $atProtoPath -Recurse -Filter *.json |
+    ForEach-Object {
+        $json = Get-Content -Path $_.FullName -Raw 
+        $jsonObject = ConvertFrom-Json -InputObject $json
+        if (-not $jsonObject.id) { return }
+        
+        $json > "$($jsonObject.id).json"
+        $AllLexicons += $jsonObject
+    }
+
+
+$AllLexicons | 
+    ForEach-Object -Begin {
+        "<ul class='atLexicons'>"        
+    } -Process {
+        "<li>"
+        "<a href='$($_.id).json'>$($_.id)</a>"
+        "</li>"
+    } -End {
+        "</ul>"
+    }
+
+
+Pop-Location
