219d9dc8010415d7dda1e15a89ce3352dbe37296
James Brundage <+@noreply.github.com>
2025-05-22 23:30:35 -0700

    feat: https://MrPowerShell.com/At/Lexicons ( Fixes #32 )
    Adding community.lexicon

--- MrPowerShell.com/At/Lexicons/index.html.ps1
+++ MrPowerShell.com/At/Lexicons/index.html.ps1
@@ -3,9 +3,27 @@
 # We'll be populating a subdirectory with a sparse checkout of a repository.
 if (-not $psScriptRoot) { return }
 
-$atProtoPath = Join-Path ($PSScriptRoot | Split-Path | Split-Path | Split-Path) atproto
+$greatGreatGrandParent = $PSScriptRoot | Split-Path | Split-Path | Split-Path
 
-Push-Location $PSScriptRoot
+$atProtoPath = Join-Path $greatGreatGrandParent atproto
+$lexiconCommunityPath = Join-Path $greatGreatGrandParent lexicons.community
+
+# If we don't have the content, get it.
+if (-not (Test-Path $lexiconCommunityPath)) {
+    $gitOutput = @(
+        # use ugit to clone -Nothing
+        # git clone https://github.com/bluesky-social/atproto/ -Nothing
+        git clone --depth 1 --no-checkout --sparse --filter=tree:0 https://github.com/lexicon-community/lexicon $lexiconCommunityPath
+        Push-Location $lexiconCommunityPath
+        # and then use a sparse-checkout to get only the CSS-related content.
+        git sparse-checkout set --no-cone /community/lexicon/**/**.json
+        # checkout the content, and we're set.
+        git checkout
+        
+        Pop-Location
+    )
+}
+
 # If we don't have the content, get it.
 if (-not (Test-Path $atProtoPath)) {
     $gitOutput = @(
@@ -24,7 +42,7 @@ if (-not (Test-Path $atProtoPath)) {
 
 $AllLexicons = @()
 
-Get-ChildItem -Path $atProtoPath -Recurse -Filter *.json |
+Get-ChildItem -Path $atProtoPath, $lexiconCommunityPath -Recurse -Filter *.json |
     ForEach-Object {
         $json = Get-Content -Path $_.FullName -Raw 
         $jsonObject = ConvertFrom-Json -InputObject $json
@@ -37,7 +55,7 @@ Get-ChildItem -Path $atProtoPath -Recurse -Filter *.json |
 
 $AllLexicons | 
     ForEach-Object -Begin {
-        "<ul class='atLexicons'>"        
+        "<ul class='atLexicons'>"
     } -Process {
         "<li>"
         "<a href='$($_.id).json'>$($_.id)</a>"
