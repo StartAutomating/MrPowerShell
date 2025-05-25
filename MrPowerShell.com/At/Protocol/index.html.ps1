ae5e4ebf279ce54d832809095cfa67df4b529ae9
James Brundage <+@noreply.github.com>
2025-05-25 16:10:54 -0700

    chore: https://MrPowerShell.com/At/Protocol
    Moving stuff around

--- MrPowerShell.com/At/Protocol/index.html.ps1
+++ MrPowerShell.com/At/Protocol/index.html.ps1
@@ -1,3 +1,4 @@
+ConvertFrom-Markdown -InputObject @'
 ## At Protocol
 
 I have something of a passion for At Protocol.
@@ -5,3 +6,5 @@ I have something of a passion for At Protocol.
 In case you don't know, [At Protocol](https://atproto.com/guides/overview) is a protocol for social media, and it's the foundation of [BlueSky](https://bsky.app).
 
 At Protocol provides a standard layer for social media, and is great to build upon.
+'@ | 
+    Select -ExpandProperty HTML
--- MrPowerShell.com/git.filters.ps1
+++ MrPowerShell.com/git.filters.ps1
@@ -1,20 +1,78 @@
-function git.sparse
+function Get-GitSparse
 {
+    [Alias('git.sparse','GitSparse')]
     param(
-        [string]
+        [Parameter(Mandatory)]
+        [uri]
         $Repository="https://github.com/bluesky-social/atproto/",
+
         [string]
-        $Path="atproto",
+        $Path,
+
         [string[]]
-        $Pattern=@('lexicons/**/**.json')
+        $Pattern=@('lexicons/**/**.json'),
+
+        [Alias('AsHashTable')]
+        [switch]
+        $AsDictionary
     )
 
+    begin {
+        filter AsDictionary {
+            $outputDictionary = [Ordered]@{}
+            foreach ($file in Get-ChildItem -Recurse -File) {
+                $relativePath = $file.FullName -replace 
+                    "^$([Regex]::Escape("$pwd"))" -replace 
+                    '\\', '/' -replace 
+                    '/{1,}', '/' -replace
+                    '^/+' 
+                $outputDictionary["/$relativePath"] = 
+                    switch -regex ($file.Extension) {
+                        '\.json$' {
+                            Get-Content -Path $file.FullName -Raw | ConvertFrom-Json
+                        }
+                        '\.jsonl$' {
+                            Get-Content -Path $file.FullName | ConvertFrom-Json
+                        }
+                        '\.ps1$' {
+                            $ExecutionContext.SessionState.InvokeCommand.GetCommand($file.FullName, 'ExternalScript')
+                        }
+                        '\.(?:ps1)?xml$' {
+                            (Get-Content -Path $file.FullName -Raw) -as [xml]
+                        }
+                        '\.psd1' {
+                            Import-LocalizedData -FileName $file.Name -BaseDirectory $file.Directory.FullName
+                        }
+                        default {
+                            Get-Content -Path $file.FullName -Raw
+                        }
+                    }
+            }
+            
+            $outputDictionary
+        }
+    }
+
     process {
-        if (Test-Path $path) { return }
-        git clone --depth 1 --no-checkout --sparse --filter=tree:0 $Repository $Path
+        if (-not $PSBoundParameters['Path']) {
+            $Path = $PSBoundParameters['Path'] = $Repository.Segments[-1] -replace '\.git$'
+        }
+        if (Test-Path $path) { 
+            if ($AsDictionary) {
+                Push-Location $path
+                AsDictionary
+                Pop-Location
+            }
+                        
+            return 
+        }
+        $null = git clone --depth 1 --no-checkout --sparse --filter=tree:0 $Repository $Path
         Push-Location "./$Path"
-        git sparse-checkout set --no-cone @Pattern
-        git checkout
+        $null = git sparse-checkout set --no-cone @Pattern
+        $null = git checkout
+        if ($AsDictionary) {
+            AsDictionary
+        }
         Pop-Location
     }
 }
