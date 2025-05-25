aca10ed5fa3072ef54389d2498b6318362216227
James Brundage <+@noreply.github.com>
2025-05-25 11:37:05 -0700

    feat: Adding Version ( Fixes #36 )
    Adding Module Versions table

--- MrPowerShell.com/Version.html.ps1
+++ MrPowerShell.com/Version.html.ps1
@@ -1,11 +1,17 @@
 $Version = [Ordered]@{} + $PSVersionTable
 $Version.Modules = [ordered]@{}
+"<h3>PSVersionTable</h3>"
 "<table>"
 foreach ($versionProperty in $PSVersionTable.GetEnumerator()) {
     "<tr><td>$($versionProperty.Key)</td><td>$($versionProperty.Value)</td></tr>"    
 }
+
+"</table>"
+"<table>"
+"<thead><tr><th>Module</th><th>Version</th></tr></thead>"
 foreach ($loadedModule in Get-Module) {
     $Version.Modules[$loadedModule.Name] = $loadedModule.Version
+    "<tr><td>$($loadedModule.Name)</td><td>$($loadedModule.Version)</td></tr>"    
 }
 "</table>"
 $Version | ConvertTo-Json -Depth 5 > ./Version.json
