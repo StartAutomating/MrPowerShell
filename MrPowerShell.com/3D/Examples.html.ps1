b198021fbcc8abfa8913195a89ca87cc6dc14cce
James Brundage <+@noreply.github.com>
2025-05-26 15:10:24 -0700

    feat: https://MrPowerShell.com/3D/Examples
    Expanding hierarchy

--- MrPowerShell.com/3D/Examples.html.ps1
+++ MrPowerShell.com/3D/Examples.html.ps1
@@ -5,27 +5,32 @@ $htmlFiles = $3jsFiles |
     Where-Object Extension -eq '.html'
     
 $pageDepth = 0
+$inSegments = @()
 $root = "$pwd"
 
 "<h2>Three.js Examples</h2>"
 
-"<p>This page clones the [three.js](https://github.com/mrdoob/three.js) examples</p>"
+"<p>This page clones the <a href='https://github.com/mrdoob/three.js'>three.js</a> examples</p>"
 
 "<details>"
 "<summary>View Source</summary>"
 "<pre><code class='language-PowerShell'>$([Web.HttpUtility]::HtmlEncode($MyInvocation.MyCommand.ScriptBlock))</code></pre>"
 "</details>"
-
+"<hr/>"
 foreach ($file in $htmlFiles) {
+    if ($file.Name -eq 'index.html') { continue }
     $fileSegments = @($file.Name -split '[/_]')
-    if ($fileSegments.Length -gt $pageDepth) {
-        $pageDepth = $fileSegments.Count
+    $parentSegments = $fileSegments[0..($fileSegments.Count - 2)]
+           
+    if ("$parentSegments" -ne "$inSegments") {
+        if ($inSegments) {
+            "</ul>"
+            "</details>"
+        }
+        $inSegments = $parentSegments
+        "<details open>"
+        "<summary>$($parentSegments -join ' / ')</summary>"
         "<ul>"
-    }
-
-    if ($fileSegments.Length -lt $pageDepth) {        
-        "</ul>"
-        $pageDepth = $fileSegments.Count
     }
     "<li>"
     "<a href='$($file.FullName.Substring($root.Length))'>$($file.Name -replace '\.html$')</a>"
