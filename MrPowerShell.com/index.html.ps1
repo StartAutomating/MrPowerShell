5704e3969c43d56e1faf022b7a93ff8b4989b1ea
James Brundage <+@noreply.github.com>
2025-05-29 20:22:46 -0700

    style: Updating homepage ( Fixes #45 )
    Adding mentions and tags and revising messaging

--- MrPowerShell.com/index.html.ps1
+++ MrPowerShell.com/index.html.ps1
@@ -1,4 +1,4 @@
-$TopLevelLinks = 'GitHub','Gists', 'Memes'
+$TopLevelLinks = 'GitHub', 'Gists', 'Memes', 'Mentions', 'Tags'
 
 $navigation = @($TopLevelLinks |
     ForEach-Object -Begin {
@@ -95,16 +95,25 @@ I was lucky enough to help test and build PowerShell V2 and V3 at Microsoft,
 and I've continued to do amazing things with the language ever since.
 </p>
 
-<p>
-I enjoy making hard things easy to do, and I love to share my knowledge with others.
-</p>
+<h3>
+Things To Know:
+</h3>
 
-<p>
-I'm always interested in interesting projects.
-</p>
+<ul>
+<li>I do a lot of meta-programming.</li>
+<li>I love to make hard things easy to do.</li>
+<li>I enjoy sharing my knowledge with others.</li>
+<li>I'm always interested in interesting projects.</li>
+</ul>
 
+<h3>
+What is this site?
+</h3>
 <p>
-If you need the help of a Jack of all trades and master of PowerShell, reach out.
+This site is a living experiment in PowerShell Web development.
+</p>
+<p>
+It's both a personal page and a proving ground for new ideas.
 </p>
 </div>
 "@
