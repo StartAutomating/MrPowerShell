6f3d4e8b7b673dba18bb81419906741ed5a76eba
James Brundage <+@noreply.github.com>
2025-06-08 12:00:02 -0700

    test: Breadcrumb navigation test ( Fixes #53 )
    Using <a> instead of onclick

--- MrPowerShell.com/Tests/Breadcrumb.html.ps1
+++ MrPowerShell.com/Tests/Breadcrumb.html.ps1
@@ -2,19 +2,18 @@ if ($PSScriptRoot) { Push-Location $PSScriptRoot }
 $navData = foreach ($file in Get-ChildItem | Where-Object Name -match '\.html\.ps1$') {
     $file.Name -replace '\.html\.ps1$'
 }
-"<nav>"
+"<nav id='breadcrumbBar'>"
 
 @'
-<span id='breadcrumbs'><button onclick='window.location.href="/"'>/</button></span>
+<span id='breadcrumbBar'><a href='/'><button>/</button></a></span>
 '@
 
 @'
-
 <script>
 var urlSegments = window.location.pathname.split('/')
 var breadcrumbs = document.getElementById('breadcrumbs');
 for (var i = 1; i < (urlSegments.length - 1); i++) {
-    breadcrumbs.innerHTML += `<button onclick="window.location.href='${urlSegments.slice(0, i + 1).join('/')}';">${urlSegments[i]}</button>`;
+    breadcrumbs.innerHTML += `<a href='${urlSegments.slice(0, i + 1).join('/')}' id='breadcrumb-${i}'><button>${urlSegments[i]}</button></a>`;
     if (i < (urlSegments.length - 2)) {
         breadcrumbs.innerHTML += ' > ';
     }
@@ -22,7 +21,7 @@ for (var i = 1; i < (urlSegments.length - 1); i++) {
 </script>
 '@
 
-"<label for='filePicker'>/</label>"
+"<label for='filePicker'> &gt; </label>"
 "<input list='fileList' id='filePicker' name='filePicker' />"
 "<datalist id='fileList'>"
 foreach ($file in $navData) {
