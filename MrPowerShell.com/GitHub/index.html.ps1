014b4e65f0b8cdf6e030ab34b0572a270cff8b66
James Brundage <+@noreply.github.com>
2025-06-09 19:15:44 -0700

    feat: https://MrPowerShell.com/GitHub improvement ( Fixes #58 )

--- MrPowerShell.com/GitHub/index.html.ps1
+++ MrPowerShell.com/GitHub/index.html.ps1
@@ -1,10 +1,22 @@
 #requires -Module PSDevOps
+param(
+    [string]
+    $did = 'did:plc:hlchta7bwmobyum375ltycg5',
+
+    [string]
+    $GitHubUserName = 'StartAutomating',
+
+    [Alias('GitHubOrgs','GitHubOrg')]
+    [string[]]
+    $GitHubOrganizations = @('StartAutomating', 'PowerShellWeb')
+)
 
 $Title = "My GitHub Repos"
 
 if (-not $script:CachedRepoList) {
     $script:CachedRepoList = [Ordered]@{}    
     $connectedToGitHub = Connect-GitHub
+    $myGitHubInfo = api.github.com/users/<username> -username $GitHubUserName
     $startAutomatingRepos = api.github.com/users/<username>/repos -username StartAutomating
     $psWebRepos = api.github.com/users/<username>/repos -username PowerShellWeb
     $script:CachedRepoList['StartAutomating'] = $startAutomatingRepos
@@ -13,8 +25,24 @@ if (-not $script:CachedRepoList) {
 
 $myRepos = @($script:CachedRepoList.Values) | . { process { $_ } }
 $myRepos | ConvertTo-Json -Depth 10 | Set-Content -Path $PSScriptRoot/Repos.json
-$myReposByPopularity = $myRepos | Sort-Object stargazers_count -Descending 
+$myReposByPopularity = $myRepos | Where-Object -Not Fork | Sort-Object stargazers_count -Descending 
 $totalStars = ($myRepos | Measure-Object -Property stargazers_count -Sum).Sum
+
+if (-not $atProtocolData -and $at.AtData) {
+    $atProtocolData = $at.AtData
+}
+
+$postsAboutRepos =
+    if ($atProtocolData) {
+        foreach ($message in @($atProtocolData.Tables['app.bsky.feed.post'].Select("did = '$($did -replace '_', ':')'", "createdAt DESC")).message) {
+            $messageLink = $message.commit.record.embed.external.uri -as [uri]
+            if (-not $messageLink) { continue }
+            if ($messageLink.DnsSafeHost -match 'github\.com$') {
+                $message
+            }
+        }
+    }
+
 $markdown = @(
 "# Most of My Repos"
 
@@ -23,7 +51,9 @@ $markdown = @(
 "## Some Stats:"
 
 "  * $totalStars ★"
+"  * $($myGitHubInfo.followers) followers"
 "  * $($myRepos.Count) public repos"
+"  * $($($myRepos | Where-Object -Not Fork | Measure-Object).Count) of my repos are original"
 "  * $($($myRepos | Where-Object Fork | Measure-Object).Count) of my repos are forks"
 "  * $($($myRepos | Measure-Object -Property forks_count -Sum).Sum) forks of my repos"
 "  * $($($myRepos | Measure-Object -Property watchers_count -Sum).Sum) watchers of my repos"
@@ -35,7 +65,7 @@ foreach ($repoInfo in $myReposByPopularity) {
 }
 
 "## By Recency:"
-foreach ($repoInfo in $myRepos | Sort-Object updated_at -Descending) {
+foreach ($repoInfo in $myRepos | Where-Object Fork -Not | Sort-Object updated_at -Descending) {
 "  * [$($repoInfo.owner.login)/$($repoInfo.Name)]($($repoInfo.html_url))"
 }
 ) -join [Environment]::NewLine
