5942124b0f2307705cd649e2d2c0524ce20f18b7
James Brundage <+@noreply.github.com>
2025-07-04 13:27:48 -0700

    style: Adjusting viewport minimum-scale

--- MrPowerShell.com/layout.ps1
+++ MrPowerShell.com/layout.ps1
@@ -224,7 +224,7 @@ $headerElements = @(
         "
     }
     # * Viewport metadata
-    "<meta name='viewport' content='width=device-width, initial-scale=1' />"
+    "<meta name='viewport' content='width=device-width, initial-scale=1, minimum-scale=1.0' />"
 
     # * Open Graph metadata
     if (
