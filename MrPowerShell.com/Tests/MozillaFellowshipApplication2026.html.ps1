ea752fa63f794999aa64e5ac148af3529469eaf4
StartAutomating <james.brundage@start-automating.com>
2026-02-13 14:29:03 -0800

    feat: Mozilla Fellowship Letter of Intent ( Fixes #229 )

---
+++ MrPowerShell.com/Tests/MozillaFellowshipApplication2026.html.ps1
@@ -0,0 +1,276 @@
+<#
+.SYNOPSIS
+    Mozilla Fellowship Application
+.DESCRIPTION
+    Mozilla Fellowship Application for 2026
+#>
+
+<#
+Protecting User Privacy
+Auditing AI Systems
+Advancing Climate Justice
+Protecting Creators
+Democratizing Data
+Building Open Infrastructure
+Other (Building Open Infrastructure / Educational )
+
+#>
+
+$sections = [Ordered]@{
+    "Why this focus area?  What unique perspective or approach do you bring to this challenge? (400 characters)" = @'
+Making open infrastructure easier for everyone has been my life's work for decades.
+
+I helped build PowerShell and bring the ethos of easy to millions.
+
+I have been developing since before the dawn of the internet.
+
+I am bringing back old learning tools like Turtle and moderinizing them.
+
+I hope to carry the torch of knowledge forward to the next generation, and make webdev simple, easy, and fun.
+'@
+    "Fellowship Motivation:
+
+Mozilla Fellows are the kind of leaders a better technology future demands.
+
+How do you hope the Fellowship will advance your evolution as a tech leader?
+
+What do you hope to gain from this experience? (800 characters)
+
+" = @'
+
+I love helping people learn and grow.  I love building tools for everyone.
+
+If I could, I would devote every day to altruism.
+
+The Mozilla Fellowship would be life-changing!
+
+It would give me the financial security to devote a full year to public good.
+
+It would provide a platform to share this good with the world.
+
+It would also introduce me to a new cohort of colleagues and allies (hopefully also driven by altruism)
+
+My greatest hope is that it would effectuate a shift in my career towards altruism.
+
+I prefer to use my talents to help people learn and grow, rather than help companies improve the bottom line.
+
+'@
+
+@'
+Cohort Contribution:
+What would you bring to a global cohort of technology leaders working toward better futures?
+How would you contribute to your fellow Fellows' growth and success? (500 characters)
+'@ = @'
+
+I would bring decades of development experience and historical understanding of the tech industry into the future.
+
+I have a deep understanding of operating systems, web standards, language design, software engineering, logic, and mathematics.
+
+I am always eager to help people learn, grow, and succeed.
+
+I hope to share as much understanding as I can with my fellow Fellows and the rest of the world.
+
+'@
+
+@'
+Most Relevant Professional Experience:
+highlight 2-3 examples of your work that best demonstrate you're ready for this Fellowship.
+Focus on projects where you had significant impact or leadership responsibility. (1,200 characters)
+'@ = @'
+
+I believe the vast majority of my career demonstrates my readiness, and picking a few projects gives only a small slice of the story.
+
+During the past 5 years alone I have released over 40 projects and made more than 10000 contributions to open source projects.
+
+I am a pretty prolific programmer: I normally produce upwards of 1mb of new scripts a year.
+
+I am lucky to have extraordinary impact in my career.
+
+I am most proud of my work on PowerShell (2006-2010 at Microsoft, 2010-Present as a consultant).
+
+I helped make the language easy to use, and was responsible for sheparding adoption by partner teams.
+
+I also evangelized the language, provided public guidance, and have brought the language to sector after sector ever since.
+
+Most notably, I created the first client UI libraries in PowerShell, and have been doing web dev with PowerShell since 2011.
+
+Most recently (and pertinently), I rebuilt Turtle within PowerShell last year, and released the alpha of Reptile earlier this year.
+
+This means that unlike many potential projects the Fellowship may be considering, I already have functioning public Minimum Viable Products.
+
+See: https://psturtle.com/
+
+'@
+
+@'
+Technical Expertise:
+what technical skills and experiences do you have that best equip you to carry out your proposed Fellowship project? (400 characters)
+'@ = @'
+
+I have deep experience in language development, web development, framework development, and infrastructure as code.
+
+I know from a lifetime of experience that I can build anything.
+
+I have been a webdev since the dawn of the Internet and know web standards well.
+
+I know the past and present of computing well, and hope to bring that knowledge into the future.
+'@
+
+@'
+Public Engagement & Communication:
+please describe your experience sharing your work with broader audiences (e.g., writing, speaking, teaching).
+How do you make complex technical concepts accessible to diverse audiences? You can include links. (600 characters)
+'@ = @'
+
+I have been evangelizing technology nearly as long as I have been creating it.
+
+I have extensive experience in public speaking.  I have spoken at roughly 2-3 conferences/year for the past 16 years.
+
+I run user groups and enjoy platforming new speakers.
+
+I build content creation tooling, and creating more content all the time.
+
+I am always looking for new ways to help people learn and grow, and enjoy tailoring the message to each audience and individual. 
+'@
+
+@'
+Project Title (100 characters)
+'@ = @'
+Turtle & Reptile
+'@
+
+@'
+Project Summary:
+clearly outline the project you wish to undertake as part of your Mozilla Fellowship.
+What will you build or create? What challenge does it address?  (1,000 characters)
+'@ = @'
+
+Last year I rebuilt Turtle into a web friendly language.
+
+Earlier this year, I released an alpha of Reptile, a framework for making safe interactive REPLs.
+
+I intent to make Turtle & Reptile even easier to use and understand.
+
+I intend to produce Educational REPLs for different age groups (Pre-K-12), so that they can learn math, science, computing, and web development.
+
+This addressees the perpetual problem of learning to program, by making it more easy and accessible.
+
+This also aims to address systemic inequality in technology, by letting anyone learn creatively at their own pace, free of social pressures.
+
+Last but not least, this hopes to address the difficulty curve of web development by making it easier for everyone.
+
+'@
+
+@'
+Vision Alignment:
+how does your proposed project advance Mozilla Foundations's vision of technology 
+that is "built with care, powered by people, fueled by imagination"? (600 characters)
+'@ = @'
+
+I first encountered the wonders of computing as a child by playing with Turtle on the Apple II.
+
+It was my and many others' first introduction to programming and graphics, and it made it easy and fun.
+
+Development has gotten quite a bit more complicated over the years.
+
+As I've told many mentees wanting to learn:  There is no "shallow end" of the pool anymore.
+
+I will help bring things back to their roots and make web development easy enough a child can wade in and begin to learn how to swim.
+
+'@
+
+@'
+Community-Centered Approach:
+who are the communities most affected by the challenge or issue area your project is designed to address?
+How will you connect to, listen to, and learn from these communities? (600 characters)
+'@ = @'
+
+The children of the world stand to benefit the most from learning technology.
+
+All seeking knowledge are welcome, and those with the most to learn may benefit the most.
+
+I have been doing limited beta testing of the projects across age groups,
+and intend to eagerly absorb any feedback from anyone who will give it.
+
+I will be reaching out to schools and charities to find more communities in need.
+
+I look forward to learning what makes things easier for each community. 
+'@
+
+@'
+Success Metrics:
+how will the impact of your proposed project be measured?
+Please include both quantitative and qualitative indicators in your response. (500 characters)
+'@ = @'
+The more people that learn and grow, the more successful I will consider the project.
+
+I will consider the project a greater success if:
+
+* The pilot program is expanded to more than 1000 users
+* Any schools plan to bring Turtle back into the classroom.  
+* Turtle exceeds 50,000 downloads
+* Turtle is used to build over 100 websites
+* Turtle/Reptile are localized to at least one other language
+* Turtle/Reptile is cross-platform (check) and easy to install.
+
+'@
+
+@'
+Project Milestones:
+please provide a high level quarterly timeline for your proposed Fellowship project,
+detailing key milestones and anticipated deliverables.
+Please note that all proposed project milestones and deliverables are subject to 
+potential revision in consultation with Mozilla and/or 
+Track I Host Organizations if you are selected for the Fellowship. (500 characters)
+'@ = @'
+
+By Q1 I would like to have Turtle Reptiles for five distinct age ranges (Pre-K, K-8, 8-12, 12-18, 18+)
+
+By Q2 I would like to have identified target partner institutions and localized the tool.
+
+By Q3 I would like to have Turtle Reptiles for each grade and an affordable prototype
+
+By Q4 I hope to have partnered with several schools to help teach technology to the next generation.
+
+'@
+}
+
+$totalChars = 0
+$maxChars =0 
+foreach ($section in $sections.Keys) {
+    if (-not $sections[$section]) {
+        Write-Host "missing: $section" -ForegroundColor Yellow
+        continue
+    }
+    if ($section -match '(?<chars>[\d,]+) char') {
+        $charCount = $matches.chars -replace ',' -as [int]
+        $maxChars += $charCount
+        $totalChars += $sections[$section].Length
+        if ($sections[$section].Length -le $charCount) {
+            Write-Host ($charCount - $sections[$section].Length) -ForegroundColor Green
+            Write-Host $section -ForegroundColor Green
+
+            Write-Host $sections[$section]
+
+            "<blockquote>$([Web.HttpUtility]::HtmlEncode($section))</blockquote>"
+
+            ConvertFrom-Markdown -InputObject $sections[$section] | 
+                Select-Object -Expand Html
+            
+        } else {
+            Write-Host ($charCount - $sections[$section].Length) -ForegroundColor Red
+            Write-Host $section -ForegroundColor Red
+
+            "<blockquote>$([Web.HttpUtility]::HtmlEncode($section))</blockquote>"
+
+            ConvertFrom-Markdown -InputObject $sections[$section] | 
+                Select-Object -Expand Html
+
+            Write-Host $sections[$section]
+            
+        }
+    }
+}
+
+Write-Host "Total Characters $totalChars"
+Write-Host "Max Characters $maxChars"
