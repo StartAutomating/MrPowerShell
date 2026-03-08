13d78b1b8e95d611d8dbcbb829246ac18ae20f54
StartAutomating <james.brundage@start-automating.com>
2026-03-07 21:04:12 -0800

    feat: https://mrpowershell.com/xrpc/site.standard.publication ( Fixes #244 )

---
+++ MrPowerShell.com/xrpc/site.standard.publication/index.json.ps1
@@ -0,0 +1,7 @@
+[PSCustomObject]@{
+    '$type' = 'site.standard.publication'
+    PSTypeName = 'site.standard.publication'
+    url = 'https://MrPowerShell.com/'
+    name = "MrPowerShell"
+    description = "Jack of all trades, master of PowerShell.  Personal webpage and ever-evolving internet experiment"
+}
