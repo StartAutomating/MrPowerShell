d61eab607535ef5ac243bccfd01466d99b2ceef6
StartAutomating <james.brundage@start-automating.com>
2026-03-07 20:41:53 -0800

    feat: https://mrpowershell.com/Modules ( re #243 )
    Using XRPC in Modules page

--- MrPowerShell.com/Modules/index.html.ps1
+++ MrPowerShell.com/Modules/index.html.ps1
@@ -11,9 +11,9 @@
     https://MrPowerShell.com/Modules
 #>
 param(
-    [Alias('GalleryCondition')]
+    [Alias('PowerShellGalleryCondition')]
     [string[]]
-    $GalleryConditions = @(
+    $PowerShellGalleryConditions = @(
         "CompanyName eq 'Start-Automating'",
         "CompanyName eq 'Start Automating'",         
         "Authors eq 'James Brundage'",
@@ -23,11 +23,11 @@ param(
         "Id eq 'ShowUI'"
     ),
 
-    [switch]
-    $HideGalleryTotal,
-    
     [int]
-    $Skip = 0
+    $PowerShellGallerySkip = 0,
+
+    [switch]
+    $HideGalleryTotal
 )
 
 $title = "My PowerShell Modules"
@@ -36,101 +36,43 @@ $description = "My PowerShell modules published to the PowerShell Gallery."
 $fullUrl = (
     "https://www.powershellgallery.com/api/v2/Packages()?" +
         "`$filter=IsAbsoluteLatestVersion eq true and ($($GalleryConditions -join ' or '))" +
-        "&`$skip=$Skip&`$top=100&`$orderby=LastUpdated desc"
+        "&`$skip=$GallerySkip&`$top=100&`$orderby=LastUpdated desc"
 )
 
+if ($psScriptRoot) { Push-Location $PSScriptRoot}
 
+if (-not $xrpcCache) { $xrpcCache = [Ordered]@{} }
 
-
-if (-not $script:MyModuleGalleryInfoCache) {
-    $script:MyModuleGalleryInfoCache = [Ordered]@{}
+if (-not $xrpcCache['com.mrpowershell.modules']) {
+    $xrpcCache['com.mrpowershell.modules'] = . ../xrpc/com.mrpowershell.modules/index.json.ps1
 }
 
-if (-not $script:MyModuleGalleryInfoCache[$fullUrl]) {
-    $script:MyModuleGalleryInfoCache[$fullUrl] = Invoke-RestMethod $fullUrl
-}
-$moduleList = $script:MyModuleGalleryInfoCache[$fullUrl]
+$moduleList = $xrpcCache['com.mrpowershell.modules']
 
-if ($script:MyModuleGalleryInfoCache[$fullUrl] -is [xml]) {
-    $script:MyModuleGalleryInfoCache[$fullUrl].Save("$psScriptRoot/index.xml")
-}
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName Name -Value {
-    $this.properties.ID
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName CompanyName -Value {
-    $this.properties.CompanyName
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName Downloads -Value {
-    $this.properties.DownloadCount.'#text' -as [int]
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName Description -Value {
-    $this.properties.Description
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName Version -Value {
-    $this.properties.Version -as [Version]
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName LastUpdated -Value {
-    $this.properties.LastUpdated.'#text' -as [DateTime]
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName ProjectUrl -Value {
-    if ($this.properties.ProjectUrl) {
-        $this.properties.ProjectUrl -as [Uri]
-    } elseif ($this.properties.gallerydetailsurl) {
-        $this.properties.gallerydetailsurl -as [Uri]        
-    } else {
-        "https://www.powershellgallery.com/packages/$($this.Name)/$($this.Version)" -as [Uri]
-    }
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -MemberType ScriptProperty -MemberName Authors -Value {
-    $this.properties.Authors -split ',\s{0,}'
-} -Force
-
-Update-TypeData -TypeName PowerShellGallery.Module -Force -MemberType ScriptProperty -MemberName UpdatedAt -Value {
+Update-TypeData -TypeName com.mrpowershell.module -Force -MemberType ScriptMethod -MemberName ToHtml -Value {
     param()
-    
-    $this.properties.Created.'#text' -as [DateTime]
-}
-
-Update-TypeData -TypeName PowerShellGallery.Module -Force -MemberType ScriptMethod -MemberName ToHtml -Value {
-    param()
-    $publishedAt = $this.properties.Published.'#text' -as [DateTime]
+    $publishedAt = $this.Published
     $attributes = [Ordered]@{
         'class' = 'powershell-gallery-module'
-        'data-module-name'        = $this.Name
-        'data-module-version'     = $this.Version
-        'data-module-downloads'   = $this.Downloads
-        'data-module-published'  = $publishedAt.ToString('o')
+        'data-module-name'        = $this.id        
+        'data-module-downloads'   = $this.downloadCount
+        'data-module-published'   = $this.Published    
     }
     $attributeString = @($attributes.GetEnumerator() | ForEach-Object { "$($_.Key)='$($_.Value)'" }) -join ' '
     "<div $attributeString>"    
         "<h2>"
-        "<a href='$($this.ProjectUrl)'>$($this.Name)</a>"        
+        "<a href='$($this.ProjectUrl)'>$($this.id)</a>"        
         "</h2>"
-        "<a href='https://www.powershellgallery.com/packages/$($this.Name)/' >"
-        "<img src='https://img.shields.io/powershellgallery/dt/$($this.Name)' />"
+        "<a href='https://www.powershellgallery.com/packages/$($this.Id)/' >"
+        "<img src='https://img.shields.io/powershellgallery/dt/$($this.Id)' />"
         "</a>"
         "<h3>v$($this.Version)</h3>"        
         "<h4>$([Web.HttpUtility]::HtmlEncode($this.Description))</h4>"
-        "<h5>Downloads: $($this.Downloads)</h5>"
-        "<p>Published: $($publishedAt.ToString('yyyy-MM-dd'))</p>"
+        "<h5>Downloads: $($this.downloadCount)</h5>"
+        "<p>Published: <time>$($publishedAt.ToString('yyyy-MM-dd'))</time></p>"
+        "<p>Created: <time>$($this.Created.ToString('yyyy-MM-dd'))</time></p>"
     "</div>"
 }
-
-
-$moduleList = @(foreach ($moduleInfo in $moduleList) {
-    if ($moduleInfo.pstypenames -notcontains 'PowerShellGallery.Module') {
-        $moduleInfo.pstypenames.insert(0, 'PowerShellGallery.Module')
-    }
-    $moduleInfo        
-})
 
 
 $moduleList = @($moduleList | Sort-Object -Property Downloads -Descending)
@@ -141,14 +83,14 @@ $moduleList = @($moduleList | Sort-Object -Property Downloads -Descending)
 ".powershell-gallery-sort { font-size: 1.5em; text-align: center; }"
 "h1 { text-align: center; }"
 "</style>"
-
+    
 "<h1>My PowerShell Modules</h1>"
 
 if (-not $HideGalleryTotal) {
     "<div class='powershell-gallery-total'>"
         "Total Modules: $("{0:N0}" -f ($moduleList.Count))",        
             "Total Downloads: $("{0:N0}" -f ($moduleList |
-                Measure-Object -Property Downloads -Sum |
+                Measure-Object -Property DownloadCount -Sum |
                 Select-Object -ExpandProperty Sum
             ))" -join '<br/>'
     "</div>"
@@ -156,12 +98,14 @@ if (-not $HideGalleryTotal) {
 
 "<div class='powershell-gallery-sort'>"
 "Sort by:"
-"<select id='sort-modules'>"
-    "<option value='moduleDownloads' selected>Downloads</option>"
-    "<option value='modulePublished'>Published</option>"
-    "<option value='moduleName'>Name</option>"
-    "<option value='moduleVersion'>Version</option>"
-"</select>"
+"
+<select id='sort-modules'>
+    <option value='random'>Random</option>
+    <option value='moduleDownloads' selected>Downloads</option>
+    <option value='modulePublished'>Published</option>    
+    <option value='moduleName'>Name</option>    
+</select>
+"
 
 $sortContainer = @'
 // We can sort a container of items by:
@@ -176,33 +120,17 @@ function sortContainer(event, containerClass) {
     const container = document.querySelector(containerClass);
     const children = Array.from(container.children);
     children.sort((a, b) => {
-
-        // Random sorts are just a 50/50 chance to be sorted
-        if (sortBy === 'random') { return Math.random() - 0.5; }
-        // Anything that looks like a version should be sorted as a version
-        if (sortBy.match(/Version/i)) {
-            // since versions should be sorted descending, we reverse the localeCompare result.
-            return a.dataset[sortBy].localeCompare(b.dataset[sortBy], undefined, {numeric: true}) * -1;
-        }
-
-        // Next, see if they can be parsed as dates
-        var aDate = Date.parse(a.dataset[sortBy]);
-        var bDate = Date.parse(b.dataset[sortBy]);
-        // If both are valid dates, sort by date
-        if (!isNaN(aDate) && !isNaN(bDate)) {
-            return bDate - aDate;
-        }
-
-        // Next, see if they can be parsed as numbers
-        var aNumber = Number(a.dataset[sortBy]);
-        var bNumber = Number(b.dataset[sortBy]);
-        // If both are valid numbers, sort by number
-        if (!isNaN(aNumber) && !isNaN(bNumber)) {
-            return bNumber - aNumber;
-        }
-
-        // Finally, sort as strings
-        return a.dataset[sortBy].localeCompare(b.dataset[sortBy]);
+        if (sortBy === 'moduleName') {
+            return a.dataset.moduleName.localeCompare(b.dataset.moduleName)
+        } else if (sortBy === 'moduleDownloads') {
+            return parseInt(b.dataset.moduleDownloads) - parseInt(a.dataset.moduleDownloads)
+        } else if (sortBy === 'moduleCreated') {
+            return parseInt(b.dataset.moduleCreated) - parseInt(a.dataset.moduleCreated)
+        } else if (sortBy === 'modulePublished') {
+            return new Date(b.dataset.modulePublished) - new Date(a.dataset.modulePublished)
+        } else if (sortBy === 'random') {
+            return Math.random() - 0.5;
+        }        
     })
 
     for (let i = 0; i < children.length; i++) {
@@ -228,7 +156,17 @@ foreach ($moduleInfo in $moduleList) {
 }
 "</div>"
 
-# irm "https://www.powershellgallery.com/api/v2/Packages()?`$filter=IsAbsoluteLatestVersion eq true and CompanyName eq 'Start-Automating'&`$skip=$Skip&`$top=100&`$orderby=LastUpdated desc"
+#region View Source
+"<details><summary>View Source</summary>"
+"<pre><code class='language-powershell'>"
+[Web.HttpUtility]::HtmlEncode($MyInvocation.MyCommand.ScriptBlock)
+"</code></pre>"
+"</details>"
+#endregion View Source
 
+if ($site.Includes.CopyCode) {
+    . $site.Includes.CopyCode
+}
 
+if ($psScriptRoot) { Pop-Location}
 
