5659179c704406ce5d9152dea59bbac595d5be86
James Brundage <+@noreply.github.com>
2026-05-10 14:42:13 -0700

    fix: `_includes/Feather` updates ( Fixes #257 )

--- MrPowerShell.com/_includes/Feather.ps1
+++ MrPowerShell.com/_includes/Feather.ps1
@@ -1,17 +1,42 @@
+<#
+.SYNOPSIS
+    Includes Feather Icons
+.DESCRIPTION
+    Includes a feather icon in the site.
+.NOTES
+    Icons will be cached in memory to avoid repeated CDN requests.
+.EXAMPLE
+    . $site.Includes.Feather "clipboard"
+.LINK
+    https://feathericons.com/
+#>
 param(
+# The feather icon name
 [string]
-$Icon = 'chevron-right'
+$Icon = 'chevron-right',
+[uri]
+$FeatherCDN = "https://cdn.jsdelivr.net/gh/feathericons/feather@latest/icons/"
 )
 
 if (-not $script:FeatherIconCache) {
     $script:FeatherIconCache = [Ordered]@{}
 }
-$icon = $icon.ToLower() -replace '\.svg$'
 
-if (-not $script:FeatherIconCache[$icon]) {
-    $script:FeatherIconCache[$icon] = Invoke-RestMethod "https://cdn.jsdelivr.net/gh/feathericons/feather@latest/icons/$Icon.svg"
+$iconUri =
+    (
+        $FeatherCDN -replace '^https?://' -replace '^',
+            'https://' -replace '/$'
+    ), (
+        $icon.ToLower() -replace '\.svg$' -replace '^/' -replace '$' -replace '\s',
+            '-' -replace '$', '.svg'
+    ) -join '/'
+
+if (-not $script:FeatherIconCache[$iconUri]) {
+    $script:FeatherIconCache[$iconUri] = try {
+        Invoke-RestMethod $iconUri
+    } catch {
+        Write-Warning "Could not get $iconUri : $_"
+    }
 }
 
-$script:FeatherIconCache[$icon].OuterXml
-
- 
+$script:FeatherIconCache[$iconUri].OuterXml
