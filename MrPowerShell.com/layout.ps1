95e64302d3f8a4a6c6b4c0e2a0aaabf50497ca60
James Brundage <+@noreply.github.com>
2026-07-24 17:43:27 -0700

    style: Reducing `<header>` height ( Fixes #263 )

--- MrPowerShell.com/layout.ps1
+++ MrPowerShell.com/layout.ps1
@@ -14,25 +14,16 @@ param(
     # The name of the palette to use.
     [Alias('Palette')]
     [string]
-    $PaletteName = $(
-        if ($Site -and $Site['PaletteName']) { $Site['PaletteName'] }
-        else { 'Konsolas' }    
-    ),
+    $PaletteName = 'Konsolas',
 
     # The Google Font name
     [Alias('FontName')]
     [string]
-    $Font = $(
-        if ($Site -and $Site['FontName']) { $Site['FontName'] }
-        else { 'Roboto' }
-    ),
+    $Font = 'Roboto',
 
     # The Google Code Font name
     [string]
-    $CodeFont = $(
-        if ($Site -and $Site['CodeFontName']) { $Site['CodeFontName'] }
-        else { 'Inconsolata' }
-    ),
+    $CodeFont = 'Inconsolata',
     
     # The urls for any fav icons.
     [string[]]
@@ -42,59 +33,83 @@ param(
     # The key should be the icon name or content, and the value should be the URL.
     # SVG icons should be included inline so they may be stylized.
     [Collections.IDictionary]
-    $Taskbar = $(        
-        if ($page -and $page['Taskbar']) { $page.Taskbar }
-        elseif ($Site -and $site['Taskbar']) { $site['Taskbar'] }
-        else { [Ordered]@{} }
-    ),
+    $Taskbar = [Ordered]@{},
 
     # The header menu.
     [Collections.IDictionary]
-    $HeaderMenu = $(
-        if ($page -and $page.'HeaderMenu' -is [Collections.IDictionary]) {
-            $page.'HeaderMenu'
-        } elseif ($Site -and $site.'HeaderMenu' -is [Collections.IDictionary]) {
-            $site.'HeaderMenu'
-        } else {
-            [Ordered]@{}
-        }
-    ),
+    $HeaderMenu = [Ordered]@{},
 
     # The footer menu.
     [Collections.IDictionary]
-    $FooterMenu = $(
-        if ($page -and $page.'FooterMenu' -is [Collections.IDictionary]) {
-            $page.'FooterMenu'
-        } elseif ($Site -and $site.'FooterMenu' -is [Collections.IDictionary]) {
-            $site.'FooterMenu'
-        } else {
-            [Ordered]@{}
-        }
-    )
+    $FooterMenu = [Ordered]@{}
 )
 
 # The literal first thing we do is to capture the arguments and input.
 # This is important beecause `$input` can only be read once.
 $allInput = @($input)
+
 $allArguments = @($args)
 $argsAndinput = @($args) + @($allInput)
 
 #region Initialize Site and Page
+
+# Site and Page are generally expected to be an ordered dictionary
+# If they already existed, we don't have to default the values
+# (and we can use whatever they provided)
 if (-not $Site) { $Site = [Ordered]@{} }
 if (-not $page) { $page = [Ordered]@{} }
 if (-not $page.MetaData) { $page.MetaData = [Ordered]@{} }
 #endregion Initialize Site and Page
+
+#region Bind Site and Page Data
+
+# If the `$page` or the `$site` defines a layout parameter
+# we want to use that.
+$psVariable = $ExecutionContext.SessionState.PSVariable
+:nextParameter foreach ($parameterName in $MyInvocation.MyCommand.Parameters.Keys) {
+    $parameter = $MyInvocation.MyCommand.Parameters[$parameterName]        
+    if ($page.($Parameter.Name)) {
+        try {
+            $psVariable.Set($Parameter.Name, $page.($ParameterName))
+            continue nextParameter
+        } catch {
+            Write-Warning "Could not bind $($page.($parameterName)) to `$$($parameterName): $_"
+        }
+    }
+    elseif ($site.($Parameter.Name)) {
+        try {
+            $psVariable.Set($Parameter.Name, $site.($ParameterName))
+            continue nextParameter
+        } catch {
+            Write-Warning "Could not bind $($site.($parameterName)) to `$$($parameterName): $_"
+        }
+    }
+
+    foreach ($alias in $parameter.Aliases) {
+        if ($page.$alias) {
+            try {
+                $psVariable.Set($Parameter.Name, $page.$alias)
+                continue nextParameter
+            } catch {
+                Write-Warning "Could not bind $($page.$alias) to `$$($parameterName): $_"
+            }
+        }
+        elseif ($site.$alias) {
+            try {
+                $psVariable.Set($Parameter.Name, $site.$alias)
+                continue nextParameter
+            } catch {
+                Write-Warning "Could not bind $($site.$alias) to `$$($parameterName): $_"
+            }            
+        }   
+    }
+}
+#endregion Initialize Parameters
         
 #region Initialize Metadata
-$page.MetaData['og:title'] =
-    if ($title) { $title }
-    elseif ($Page.title) { $Page.title } 
-    elseif ($site.title) { $site.title }
 
-$page.MetaData['og:description'] =
-    if ($description) { $description }
-    elseif ($page.description) { $page.description }
-    elseif ($site.description) { $site.description }
+$page.MetaData['og:title'] = $title
+$page.MetaData['og:description'] = $description
 
 $page.MetaData['og:image'] =
     if ($image) { $image } 
@@ -129,12 +144,21 @@ body {
     max-width: 100vw;
     height: 100vh;
     font-family: '$Font', sans-serif;
-    margin: 1em;
+    margin: 1em;    
 }
 
 header, footer {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-areas:
+        "grid-left grid-center grid-right";
     text-align: center;
+    
 }
+
+.grid-left { grid-area: grid-left; place-items: left; }
+.grid-center { grid-area: grid-center; place-items: center;  }
+.grid-right { grid-area: grid-right; place-items: right  }
 
 article {
     background-color: var(--background);
@@ -143,22 +167,24 @@ article {
     max-width: 100%;    
 }
 
-header > svg {
-    display: block;
+header svg {    
     text-align: center;
+}
+header h1 {
+    display: inline-block;
 }
 
 $(
     if ($HeaderMenu) {
         # If the device is in landscape mode, use larger padding and gaps
         "@media (orientation: landscape) {"
-            ".header-menu { display: grid; grid-template-columns: repeat(auto-fit, minmax(100px, 1fr)); gap: 0.5em }"
+            ".header-menu { display: grid; grid-template-columns: repeat(auto-fit, minmax(100px, 1fr)); gap: 0.25rem }"
             ".header-menu-item { text-align: center; padding: 0.5em; }"
         "}"
 
         # If the device is in portrait mode, use smaller padding and gaps
         "@media (orientation: portrait) {"
-            ".header-menu { display: grid; grid-template-columns: repeat(auto-fit, minmax(66px, 1fr)); gap: 0.25em }"
+            ".header-menu { display: grid; grid-template-columns: repeat(auto-fit, minmax(66px, 1fr)); gap: 0.25rem }"
             ".header-menu-item { text-align: center; padding: 0.25em; }"
         "}"
     }
@@ -179,22 +205,32 @@ $(
 )
 
 .logo { 
-    display: inline;
+    display: inline-block;
     height: 4.2rem;
+}
+
+.logo-site-title {
+    display: flex;
+    place-items: center;    
+    flex-direction: row;    
+}
+
+.logo-site-title svg { height: 4rem }
+
+.logo svg {
+    
 }
 
 .expandInline { display: flex; flex-direction: row; }
 
-@media (orientation: landscape) {
-    .logo { height: 4.2rem; }
+@media (orientation: landscape) {    
     .site-title, .page-title {
         font-size: 1.23rem;
         line-height: .75rem
     }
 }
 
-@media (orientation: portrait) {
-    .logo { height: 2.3rem; }
+@media (orientation: portrait) {    
     .site-title, .page-title {
         font-size: 0.84em;
         line-height: .66rem
@@ -233,6 +269,10 @@ a:hover, a:focus {
 
 .taskbar * {
     vertical-align: middle;    
+}
+
+.logo {
+    display: inline-block;
 }
 
 .background {
@@ -342,44 +382,41 @@ $bodyElements = @(
         } elseif ($site.Header) {
             $site.Header -join [Environment]::NewLine
         } else {
-            "<a href='/'>"
-            @(
-                "<svg xmlns='http://www.w3.org/2000/svg' class='logo'>" + $(
+            "<section class='grid-left'>"
+                "<a href='/'>"         
+                "<h2 class='logo-site-title'>"
+                    
+                    # "<svg xmlns='http://www.w3.org/2000/svg' class='logo'>"
                     if ($site.Logo) {
                         if ($site.Logo -match '<svg') { $site.Logo -replace '<\?.+>' }
                         else { "<image src='$($site.Logo)' class='logoImage' />" }
+                    }                    
+                    # "</svg>" 
+                    if ($site.Title) {
+                        "$([Web.HttpUtility]::HtmlEncode($site.Title))"
                     }
-                ) + "</svg>"
-                if ($site.Title) {
-                    "<h1 class='site-title'>$([Web.HttpUtility]::HtmlEncode($site.Title))</h1>"
-                }
-                elseif ($site.CNAME) {                    
-                    "<h1 class='site-title'>$([Web.HttpUtility]::HtmlEncode($site.CNAME))</h1>"
-                }
-            ) -join (
-                [Environment]::NewLine + "<br/>" + [Environment]::NewLine
-            )
-            "</a>"
+                    elseif ($site.CNAME) {                    
+                        "$([Web.HttpUtility]::HtmlEncode($site.CNAME))"
+                    }                
+                "</h2>"
+                "</a>" 
+                "</section>"
+            "</section>"
+            "<section class='grid-center'>"
             if ($page.Title -and $page.Title -ne $site.Title) {
-                "<h2 class='page-title'>$([Web.HttpUtility]::HtmlEncode($page.Title))</h2>"
-            }            
+                "<h1 class='page-title'>$([Web.HttpUtility]::HtmlEncode($page.Title))</h1>"
+            }
+            "</section>"
         }
         
+        "<section class='grid-right'>"
         if ($headerMenu) {            
             "<nav class='header-menu'>"
             foreach ($menuItem in $headerMenu.GetEnumerator()) {
-                "<a href='$($menuItem.Value)' class='header-menu-item'>$([Web.HttpUtility]::HtmlEncode($menuItem.Key))</a>"
+                "<a href='$($menuItem.Value)' class='header-menu-item'>$(
+                    [Web.HttpUtility]::HtmlEncode($menuItem.Key)
+                )</a>"
             }
-            "</nav>"
-        }
-    "</header>"
-
-    # * The main content
-    "<div class='main'>$outputHtml</div>"
-
-    if ($taskbar) {
-        # * Our taskbar
-        "<div class='taskbar'>"
             foreach ($taskbarItem in $taskbar.GetEnumerator()) {
                 "<a href='$($taskbarItem.Value)' class='icon-link' target='_blank'>"
                 if ($page -and $page.Icon."$($taskbarItem.Key)") {                     
@@ -397,7 +434,35 @@ $bodyElements = @(
                 else { $taskbarItem.Key }
                 "</a>"
             }
-        "</div>"
+            "</nav>"
+        }
+        "</section>"
+    "</header>"
+
+    # * The main content
+    "<div class='main'>$outputHtml</div>"
+
+    if ($taskbar) {
+        # * Our taskbar
+        <#"<div class='taskbar'>"
+            foreach ($taskbarItem in $taskbar.GetEnumerator()) {
+                "<a href='$($taskbarItem.Value)' class='icon-link' target='_blank'>"
+                if ($page -and $page.Icon."$($taskbarItem.Key)") {                     
+                    $page.Icon[$taskbarItem.Key]
+                    if ($site.ShowTaskbarIconText -or $page.ShowTaskbarIconText) {
+                        $taskbarItem.Key
+                    }                    
+                }
+                elseif ($site -and $site.Icon."$($taskbarItem.Key)") { 
+                    $site.Icon[$taskbarItem.Key]
+                    if ($site.ShowTaskbarIconText -or $page.ShowTaskbarIconText) {
+                        $taskbarItem.Key
+                    }                
+                }
+                else { $taskbarItem.Key }
+                "</a>"
+            }
+        "</div>"#>
     }
 
     # * The footer
